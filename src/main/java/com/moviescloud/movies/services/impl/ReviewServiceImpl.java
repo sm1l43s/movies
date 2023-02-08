d15f2902c0130d9cb9f826b28cb73ad28163ce4c
Денис Браусов <brausowdenis@gmail.com>
2023-02-08 15:22:16 +0300

    fix with adding and editing a review and highlighting a separate ReviewControllerontroller

---
+++ src/main/java/com/moviescloud/movies/services/impl/ReviewServiceImpl.java
@@ -0,0 +1,45 @@
+package com.moviescloud.movies.services.impl;
+
+import com.moviescloud.movies.entities.Review;
+import com.moviescloud.movies.exceptions.ResourceNotFoundException;
+import com.moviescloud.movies.repositories.ReviewRepository;
+import com.moviescloud.movies.services.IReviewService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ReviewServiceImpl implements IReviewService {
+    final ReviewRepository reviewRepository;
+    @Autowired
+    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public Page<Review> findAll(Pageable pageable) {
+        log.info("Getting a list of reviews");
+        return reviewRepository.findAll(pageable);
+    }
+
+    @Override
+    public Review findById(Long id) {
+        log.info("Get a review by id{}", id);
+        return reviewRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Review with id="+ id +" not found"));
+    }
+
+    @Override
+    public Review save(Review review) {
+        log.info("Save a review{}", review);
+        return reviewRepository.save(review);
+    }
+
+    @Override
+    public void delete(Review review) {
+        log.info("Delete a review{}", review);
+        reviewRepository.delete(review);
+    }
+}
