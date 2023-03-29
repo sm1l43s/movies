421d2ee925d15258a4eea41cddbddfc3b4e2e0b0
Денис Браусов <brausowdenis@gmail.com>
2023-03-29 18:10:56 +0300

    test proxy

---
+++ src/main/java/com/moviescloud/movies/controllers/ProxyController.java
@@ -0,0 +1,58 @@
+package com.moviescloud.movies.controllers;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+
+@RestController
+@RequestMapping("/proxy")
+public class ProxyController {
+
+    private String server = "134.17.25.94";
+    private int port = 55052;
+    private String userInfo = "YURY:2195662Aa@";
+
+    @RequestMapping("/**")
+    public ResponseEntity mirrorRest(@RequestBody(required = false) String body,
+                                     HttpMethod method, HttpServletRequest request, HttpServletResponse response)
+            throws URISyntaxException {
+        String requestUrl = request.getRequestURI();
+
+        URI uri = new URI("http", userInfo, server, port, null, null, null);
+
+        uri = UriComponentsBuilder.fromUri(uri)
+                .path(requestUrl)
+                .query(request.getQueryString())
+                .build(true).toUri();
+
+        HttpHeaders headers = new HttpHeaders();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers.set(headerName, request.getHeader(headerName));
+        }
+
+        HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            return restTemplate.exchange(uri, method, httpEntity, String.class);
+        } catch(HttpStatusCodeException e) {
+            return ResponseEntity.status(e.getRawStatusCode())
+                    .headers(e.getResponseHeaders())
+                    .body(e.getResponseBodyAsString());
+        }
+    }
+}
