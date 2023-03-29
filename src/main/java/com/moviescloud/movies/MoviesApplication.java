a879ba5d705e61737ec763b15b72752e3bd26c0c
Денис Браусов <brausowdenis@gmail.com>
2023-03-29 16:45:18 +0300

    delete Proxy

--- src/main/java/com/moviescloud/movies/MoviesApplication.java
+++ src/main/java/com/moviescloud/movies/MoviesApplication.java
@@ -2,14 +2,10 @@ package com.moviescloud.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableRetry
 public class MoviesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
     }
-
 }
