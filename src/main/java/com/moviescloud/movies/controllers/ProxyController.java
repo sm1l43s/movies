99c8c680191065e5e1139e36e68b2aab27f4839a
Денис Браусов <brausowdenis@gmail.com>
2023-03-29 15:59:06 +0300

    test ProxyController

--- src/main/java/com/moviescloud/movies/controllers/ProxyController.java
+++ src/main/java/com/moviescloud/movies/controllers/ProxyController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,8 +27,9 @@ public class ProxyController {
 
     private String userInfo = "YURY:2195662Aa";
 
-    @RequestMapping("/**")
+    @RequestMapping("/{endpoint}")
     public ResponseEntity mirrorRest(@RequestBody(required = false) String body,
+                                     @PathVariable("endpoint") String endpoint,
                                      HttpMethod method, HttpServletRequest request, HttpServletResponse response)
             throws URISyntaxException {
         String requestUrl = request.getRequestURI();
@@ -49,7 +51,7 @@ public class ProxyController {
         HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
         RestTemplate restTemplate = new RestTemplate();
         try {
-            return restTemplate.exchange(uri, method, httpEntity, String.class);
+            return restTemplate.exchange(uri+"/"+endpoint, method, httpEntity, String.class);
         } catch(HttpStatusCodeException e) {
             return ResponseEntity.status(e.getRawStatusCode())
                     .headers(e.getResponseHeaders())
